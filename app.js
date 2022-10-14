2991f867892a26c68b2c33ce7653bb389808aa52
Destiny Boone <destaj.db@gmail.com>
2022-10-14 16:50:08 -0400

    Get & Populate Items

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ const ItemCtrl = (function(){
    // Data Structure / State
    const data = {
     items: [
-        {id: 0, namme: 'Steak Dinner', calories: 1200},
-        {id: 1, namme: 'Cookie', calories: 400},
-        {id: 2, namme: 'Eggs', calories: 300}
+        {id: 0, name: 'Steak Dinner', calories: 1200},
+        {id: 1, name: 'Cookie', calories: 400},
+        {id: 2, name: 'Eggs', calories: 300}
     ],
     currentItem: null,
     totalCalories: 0
@@ -22,17 +22,39 @@ const ItemCtrl = (function(){
 
    // Public methods
    return {
+    getItems: function(){
+        return data.items;
+    },
     logData: function(){
         return data;
     }
    }
 })();
 
-// UI Controller
+// UI Controller - takes care of anything with the user interface
 const UICtrl = (function(){
+    const UISelectors = {
+        itemList: '#item-list'
+    }
    // Public methods
     return{
+        populateItemList: function(items){
+            let html = '';
 
+            items.forEach(function(item){
+                html += `
+                <li class="collection-item" id="item-${item.id}">
+                    <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+                    <a href="#" class="secondary-content">
+                        <i class="fa fa-pencil"></i>
+                    </a>
+                </li>
+                `;
+            });
+
+            // Insert list items
+            document.querySelector(UISelectors.itemList).innerHTML = html;
+        }
    } 
 })();
 
@@ -41,7 +63,10 @@ const App = (function(ItemCtrl, UICtrl){
     // Public methods
     return{
         init: function(){ // anything we want to happen right when the application loads
-            
+            // Fetch items from data structure
+            const items = ItemCtrl.getItems();
+            // Populate list with items
+            UICtrl.populateItemList(items);
         }
     }
 })(ItemCtrl, UICtrl);
